fed418f5a122f71d0d673cc96d77aad672be9f0a
Nikita Karpin <mkarpin@mirantis.com>
2015-09-30 13:58:25 +0300

    initiate jenkins repo

---
+++ manifests/install.pp
@@ -0,0 +1,17 @@
+class jenkins::install {
+
+	package {
+    	'openjdk-7-jre':
+        	ensure      => installed
+	}
+
+##wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
+	apt::key { 'jenkinsci':
+		id  =>  '0x9B7D32F2D50582E6',
+  		source  => 'https://jenkins-ci.org/debian/jenkins-ci.org.key'
+  	}
+##sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
+  	apt::source { 'jenkinsci':
+  		location => 'http://pkg.jenkins-ci.org/debian binary/'
+	}
+}
