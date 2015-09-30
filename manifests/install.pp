98668ec590e8c4e9287b5c9da511a850e4ce933d
Nikita Karpin <mkarpin@mirantis.com>
2015-09-30 14:41:43 +0300

    fix syntax

--- manifests/install.pp
+++ manifests/install.pp
@@ -12,7 +12,7 @@ class jenkins::install {
   	}
 ##sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
   	apt::source { 'jenkinsci':
-  		location => 'http://pkg.jenkins-ci.org/debian binary/'
+  		location => 'http://pkg.jenkins-ci.org/debian binary/',
       require => apt::key['jenkinsci']
 	}
 }
