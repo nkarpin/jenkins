dce8c78aaae1a8eae5490cda4d655258fec9afc2
Nikita Karpin <mkarpin@mirantis.com>
2015-10-01 17:27:49 +0300

    add jon type

--- manifests/init.pp
+++ manifests/init.pp
@@ -20,6 +20,7 @@ class jenkins (
 	include jenkins::install
 	include jenkins::config
 	include jenkins::service
+	include jenkins::job
 
 
 	Class['jenkins::install'] ~> Class['jenkins::config'] ~> Class['jenkins::service']
