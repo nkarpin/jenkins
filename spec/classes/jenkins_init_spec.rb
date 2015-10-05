5a250ab910ea003228e64f6de3e038ddb57d13fb
Nikita Karpin <mkarpin@mirantis.com>
2015-10-05 10:05:19 +0300

    init_spec + .rspec

--- spec/classes/jenkins_init_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-describe "jenkins::init" do
-  it { should create_class('jenkins::install')}
-  it { should create_class('jenkins::config')}
-  it { should create_class('jenkins::service')}
-end
