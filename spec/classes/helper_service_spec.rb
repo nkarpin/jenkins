dc790a68807d10fb2f103a3e8d41ad40f257f515
Nikita Karpin <mkarpin@mirantis.com>
2015-10-05 17:10:40 +0300

    adding service spec

---
+++ spec/classes/helper_service_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe "jenkins::service" do
+	let(:facts) { { :lsbdistid => 'Ubuntu', :osfamily => 'Debian', :lsbdistcodename => 'trusty', :puppetversion   => Puppet.version} }
+
+	it do
+		should contain_service('jenkins').with({
+			'ensure'      => 'running',
+	        'enable'      => 'true',
+	        'require'     => 'Class[Jenkins::Config]',
+		})
+	end
+end  
