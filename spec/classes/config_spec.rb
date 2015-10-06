de3bfd14634f042cb77b13c902cef0f67ac560ca
Nikita Karpin <mkarpin@mirantis.com>
2015-10-06 14:08:06 +0300

    fix spaces

--- spec/classes/config_spec.rb
+++ spec/classes/config_spec.rb
@@ -3,15 +3,15 @@ require 'spec_helper'
 describe "jenkins::config" do
 	let(:facts) { { :lsbdistid => 'Ubuntu', :osfamily => 'Debian', :lsbdistcodename => 'trusty', :puppetversion   => Puppet.version} }
 
-    it do 
-  		should contain_file('/etc/default/jenkins').with({
-  			:ensure  => 'file',
-  		})
+	it do 
+		should contain_file('/etc/default/jenkins').with({
+			:ensure  => 'file',
+		})
 
-  		should contain_file('/var/lib/jenkins/jobs').with({
+		should contain_file('/var/lib/jenkins/jobs').with({
 			:ensure  => 'directory',
 			:mode    => '0755',
 			:owner   => 'jenkins',
-  		})
-  	end
+		})
+	end
 end  
